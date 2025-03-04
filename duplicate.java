3edc52442cf18c060e6a51b6be88eec3c5bb1d29
Barath-ash <bbarath601@gmail.com>
2025-03-05 03:34:02 +0530

    Practice questions

--- duplicate.java
+++ duplicate.java
@@ -8,7 +8,7 @@ public class duplicate {
     static int[] removeDuplicates(int[] nums) {
          int i=0;
          
-         for (int j=1;j<nums.length;i++) {
+         for (int j=1;j<nums.length;j++) {
             if(nums[j]!=nums[i]){
                 i++;
                 nums[i] = nums[j];
