27c1e67ddab2fa13d534970aee9843fdb952fa87
Barath-ash <bbarath601@gmail.com>
2025-03-05 03:32:08 +0530

    Practice questions

--- duplicate.java
+++ duplicate.java
@@ -1,20 +1,22 @@
+import java.util.Arrays;
+
 public class duplicate {
     public static void main(String[] args) {
         int[] n = {1,1,2};
-        System.out.println(removeDuplicates(n));
+        System.out.println(Arrays.toString(removeDuplicates(n)));
     }
-    static int removeDuplicates(int[] nums) {
+    static int[] removeDuplicates(int[] nums) {
          int i=0;
          
-         for (int j : nums) {
-            if(j!=nums[i]){
+         for (int j=1;j<nums.length;i++) {
+            if(nums[j]!=nums[i]){
                 i++;
-                nums[i] = j;
+                nums[i] = nums[j];
 
             }
             
          }
-         return i+1;
+         return nums;
 
 
 
