3c6d60a643dd85b0ef41bb80e06481a76e376209
Barath-ash <bbarath601@gmail.com>
2025-03-05 03:33:47 +0530

    Practice questions

--- duplicate.java
+++ duplicate.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 public class duplicate {
     public static void main(String[] args) {
         int[] n = {1,1,2};
-        System.out.println(Arrays.toString(removeDuplicates(n)));
+        System.out.println(Arrays.toString((removeDuplicates(n))));
     }
     static int[] removeDuplicates(int[] nums) {
          int i=0;
