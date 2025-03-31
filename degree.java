db81ffea120ffc6250c48e0990d1ba3a41959e95
Barath-ash <bbarath601@gmail.com>
2025-03-31 09:49:30 +0530

    Practice questions

--- degree.java
+++ degree.java
@@ -6,6 +6,12 @@ class degree {
     }
 
     static int reverseDegree(String s) {
-       
+        int sum =0;
+        int deg;
+        for(int i=0;i<s.length();i++){
+            deg= s.charAt(i)-96;
+            sum+=(27-deg)*(i+1);
+        }
+        return sum;
     }
 }
