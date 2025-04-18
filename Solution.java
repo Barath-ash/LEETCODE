d99c32890bf959afd33879cb5a1f2285fa84e871
Barath-ash <bbarath601@gmail.com>
2025-04-18 09:03:23 +0530

    Practice questions

---
+++ Solution.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+
+class Solution {
+    public static void main(String[] args) {
+
+        int nums[] = { 1, 2, 3, 4, 5, 6, 7 };
+        int k = 3;
+        rotator(nums, 0, nums.length - 1);
+        rotator(nums, 3, nums.length-1);
+        rotator(nums, 0, k-1);
+        System.out.println(Arrays.toString(nums));
+
+    }
+
+    static int[] rotator(int arr[], int start, int end) {
+          while (start<end) {
+            
+          
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+
+        return arr;
+
+    }
+
+}
