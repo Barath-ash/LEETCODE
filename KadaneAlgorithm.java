5bddc13ed6c874dbe0800b819a79b658c8f12697
Barath-ash <bbarath601@gmail.com>
2025-04-20 17:57:36 +0530

    Practice questions

---
+++ KadaneAlgorithm.java
@@ -0,0 +1,38 @@
+public class KadaneAlgorithm {
+    public static void main(String[] args) {
+        int arr[] = {1,2,-9,-7,9,7};
+
+        int max =Integer.MIN_VALUE;
+        
+        int sum=0;
+        int st=0,end=0;
+
+        for(int i=0;i<arr.length;i++){
+              
+            if(sum==0){
+                st = i;
+            }
+            
+            
+            sum+=arr[i];
+
+
+            if(sum>max){
+                max = sum;
+                end = i;
+            }
+
+
+            if(sum<1){
+                sum =0;
+            }
+        
+        }
+        System.out.println("The start is "+st);
+        System.out.println("The end is "+end);
+        System.out.println(max);
+
+
+
+    }
+}
