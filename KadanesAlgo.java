d83433d257e121abd4c5be8e4192eb8c76b87a27
Barath-ash <bbarath601@gmail.com>
2025-04-20 10:47:57 +0530

    Practice questions

--- KadanesAlgo.java
+++ KadanesAlgo.java
@@ -1,3 +1,15 @@
 public class KadanesAlgo {
+    public static void main(String[] args) {
+        int arr[] = {1,2,-9,-7,9,7};
+
+    int max =arr[0];
+    int curr = arr[0];
     
+      
+    for(int i=1;i<arr.length;i++){
+        curr = Math.max(arr[i], curr+arr[i]);
+        max = Math.max(max, curr);
+    }
+    System.out.println(max);
+    }
 }
