45a2a4633019f18a327ea1d736fe3d5f180f2efe
Barath-ash <bbarath601@gmail.com>
2025-04-20 17:59:16 +0530

    Practice questions

--- KadaneAlgorithm.java
+++ KadaneAlgorithm.java
@@ -1,6 +1,6 @@
 public class KadaneAlgorithm {
     public static void main(String[] args) {
-        int arr[] = {1,2,-9,-7,9,7};
+        int arr[] = {1,2,9,-7,9,7};
 
         int max =Integer.MIN_VALUE;
         
