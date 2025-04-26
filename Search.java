4221d7ca13b40ecbeb49efaf2b1fb663ab8c0f17
Barath-ash <bbarath601@gmail.com>
2025-04-26 08:49:21 +0530

    Practice questions

--- Search.java
+++ Search.java
@@ -1,7 +1,7 @@
 class Search{
     public static void main(String[] args) {
         int []nums = {1,3,5,6};
-        int target = 7;
+        int target = 0;
         int result =0;
         for(int i=0;i<nums.length;i++){
             if(nums[i]==target){
@@ -17,8 +17,10 @@ class Search{
                 }
             }
         }
-       
-        if(result==0){
+       if(target==0){
+        result = 0;
+       }
+       else if(result==0){
             result= nums.length;
         }
         System.out.println(result);
