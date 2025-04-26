610cfc8ad4ec41e0d9cbf4cab149efce4658f86f
Barath-ash <bbarath601@gmail.com>
2025-04-26 08:34:30 +0530

    Practice questions

---
+++ Search.java
@@ -0,0 +1,26 @@
+class Search{
+    public static void main(String[] args) {
+        int []nums = {1,3,5,6};
+        int target = 2;
+        int result =0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==target){
+                result = i;
+                break;
+            } else{
+                for(int j=0;j<nums.length;j++){
+                    if(nums[j]>target){
+                      result=j;
+                      break;
+                    }
+                    
+                }
+            }
+        }
+       
+        if(result==0){
+            result= nums.length;
+        }
+        System.out.println(result);
+    }
+}
