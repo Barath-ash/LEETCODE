3e06bac0cc5f175b83fff4458e6e6c57edc0b502
Barath-ash <bbarath601@gmail.com>
2025-04-27 18:12:11 +0530

    Practice questions

---
+++ L3100.java
@@ -0,0 +1,28 @@
+class L3100{
+    public static void main(String[] args) {
+       String s = "hello";
+       int sum =0;
+       int prev=0;
+       for(int i=0;i<s.length();i++){
+          int curr= s.charAt(i);
+           if(prev!=0) {
+            int val =  (curr-prev);
+            System.out.println(val);
+           prev = curr;
+            if(val<0){
+           val *= -1;
+             sum+=val;
+          }
+          else {
+             sum+=val;
+          }
+          }else{
+            prev = curr;
+            continue;
+          }
+         
+        
+       }
+       System.out.println(sum);
+    }
+}
--- Search.java
+++ Search.java
@@ -1,28 +1,61 @@
+import java.util.Arrays;
+
 class Search{
     public static void main(String[] args) {
-        int []nums = {1,3,5,6};
-        int target = 0;
-        int result =0;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]==target){
-                result = i;
-                break;
-            } else{
-                for(int j=0;j<nums.length;j++){
-                    if(nums[j]>target){
-                      result=j;
-                      break;
-                    }
+    //     int []nums = {1001};
+    //     int target = 5;
+    //     int result =0;
+    //     for(int i=0;i<nums.length;i++){
+    //         if(nums[i]==target){
+    //             result = i;
+    //             break;
+    //         } else{
+    //             for(int j=0;j<nums.length;j++){
+    //                 if(nums[j]>target){
+    //                   result=j;
+    //                   break;
+    //                 }
                     
-                }
-            }
+    //             }
+    //         }
+    //     }
+    //    if(target==0){
+    //     result = 0;
+    //    }
+    //    else if(nums[0]>target){
+    //     result =0
+    //    }else if(result==0){
+    //         result= nums.length;
+    //     }
+    //     System.out.println(result);
+    int nums[]={1001};
+    int target=5;
+    int i=0,j=nums.length-1;
+    int res=binaryser(nums,i,j,target);
+    if(res==-1){
+        int newarr[]=new int[nums.length+1];
+        int ind=0;
+        for(int s:nums){
+           newarr[ind++]=s;
+
         }
-       if(target==0){
-        result = 0;
-       }
-       else if(result==0){
-            result= nums.length;
-        }
-        System.out.println(result);
+        newarr[newarr.length-1]=target;
+        Arrays.sort(newarr);
+        
+    }
+    System.out.println(res);
+    }
+    public static int binaryser(int nums[],int a,int b,int target){
+    int mid=(a+b)/2;
+    if(nums[mid]==target){
+        return mid;
+    }  
+    else if(nums[mid]<target){
+        binaryser(nums, a, mid-1, target);
+    }
+    else if(nums[mid]>target){
+        binaryser(nums, mid+1, b, target);
+    }
+    return -1;
     }
 }
