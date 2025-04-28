d82122f540e73fb9e0ef890ec484f1dbfd081d0f
Barath-ash <bbarath601@gmail.com>
2025-04-28 17:49:17 +0530

    Practice questions

--- neopad_pattern.java
+++ neopad_pattern.java
@@ -1,15 +1,19 @@
-class neopad_pattern {
+class NeopadPattern {
     public static void main(String[] args) {
-        int n = 11;
+        int n = 10;
         
         // Upper half
         for (int i = n; i >= 0; i--) {
             for (int j = 0; j <= n; j++) {
                 if (j >= i) {
                     System.out.print(j);
-                   
+                    if (j < 10) {
+                        System.out.print("  "); // one number + two spaces if 0-9
+                    } else {
+                        System.out.print(" ");  // number 10 needs one space
+                    }
                 } else {
-                    System.out.print(" "); // empty space (three spaces)
+                    System.out.print("   "); // empty space (three spaces)
                 }
             }
             System.out.println();
@@ -20,11 +24,13 @@ class neopad_pattern {
             for (int j = 0; j <= n; j++) {
                 if (j >= i) {
                     System.out.print(j);
-                    if (j > 10) {
+                    if (j < 10) {
+                        System.out.print("  ");
+                    } else {
                         System.out.print(" ");
-                    }  
+                    }
                 } else {
-                    System.out.print(" ");
+                    System.out.print("   ");
                 }
             }
             System.out.println();
