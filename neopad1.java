98f6c1e2f8037a8e85368d5e5edbaf850486e13d
Barath-ash <bbarath601@gmail.com>
2025-04-28 18:17:54 +0530

    Practice questions

---
+++ neopad1.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class neopad1 {
+    public static void main(String[] args) {
+        String s = "Welcome123tomy234country34";
+        int max =0;
+        s= s.toLowerCase();
+        String[] arr = s.split("[a-z]+");
+        for(int i=0;i<arr.length;i++){
+             if(arr[i].length()!=0){
+                int val = Integer.parseInt(arr[i]);
+                if(max ==0){
+                    max =  val;
+                }
+                else if(val>max){
+                    max = val;
+                }
+                
+             }
+        }
+        System.out.println(max);
+    }
+}
