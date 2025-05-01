1f71eca31a2a4709f54f608c05d650f2515c6e35
Barath-ash <bbarath601@gmail.com>
2025-05-01 09:49:51 +0530

    Practice questions

--- wordpalindrome.java
+++ wordpalindrome.java
@@ -1,1 +1,32 @@
- 
+public class wordpalindrome {
+    public static void main(String[] args) {
+        String s = "In India Malayalam is best with dad and mom";
+        
+        String[] words = s.split("\\s+");
+        StringBuilder str  = new StringBuilder();
+        for(int i=0;i<words.length;i++){
+            if(palindrome(words[i].toLowerCase())){
+               continue; 
+
+            }
+            else{
+                str.append(words[i]);
+            }
+            if(i!=words.length-1){
+                str.append(" ");
+            }
+        }
+        System.out.println(str.toString());
+    }
+
+
+    static boolean palindrome(String s){
+        for(int i=0;i<s.length();i++){
+           if(s.charAt(i)!=s.charAt(s.length()-1-i)){
+            return false;
+            
+           }
+        }
+        return true;
+    }
+}
