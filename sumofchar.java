4ff3b389134d63089facda3cbe835a1fb5cdb746
Barath-ash <bbarath601@gmail.com>
2025-05-05 10:23:10 +0530

    Practice questions

---
+++ sumofchar.java
@@ -0,0 +1,28 @@
+class sumofchar{
+    public static void main(String[] args) {
+        String s = "iiii";
+        int k = 1;
+        System.out.println(getLucky(s, k));
+    }
+
+    static int getLucky(String s, int k) {
+        int sum = 0;
+        StringBuilder str = new StringBuilder();
+        for(int i=0;i<s.length();i++){
+             char c = s.charAt(i);
+             int val = c-'a'+1;
+              str.append(val);
+        }
+        for(int i=0;i<k;i++){
+            for(char c: str.toString().toCharArray()){
+               
+            }
+        }
+         
+             
+    
+        
+
+        return sum;
+    }
+}
