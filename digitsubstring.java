e97671c5f887a2402566758e9a0928c80f7bccb1
Barath-ash <bbarath601@gmail.com>
2025-05-27 15:25:35 +0530

    Practice questions

---
+++ digitsubstring.java
@@ -0,0 +1,9 @@
+public class digitsubstring {
+    public static void main(String[] args) {
+        String s = "123a45b67c";
+        for(int i=1;i<s.length()-1;i++){
+            int value = (int) s.charAt(i);
+            if()
+        }
+    }
+}
