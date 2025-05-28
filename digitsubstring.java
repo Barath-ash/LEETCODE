c66cf693e42fc4923d4083f5f16b4abbc3759104
Barath-ash <bbarath601@gmail.com>
2025-05-28 07:07:52 +0530

    Practice questions

--- digitsubstring.java
+++ digitsubstring.java
@@ -1,9 +1,29 @@
 public class digitsubstring {
     public static void main(String[] args) {
         String s = "123a45b67c";
-        for(int i=1;i<s.length()-1;i++){
-            int value = (int) s.charAt(i);
-            if()
-        }
+         int i=0;
+         int count =0;
+         int n= s.length();
+         while(i<n){
+            char c = s.charAt(i);
+            if(Character.isDigit(c)){
+                int start = i;
+
+                while (Character.isDigit(s.charAt(i) )) {
+                    i++;
+                }
+                int end = i-1;
+
+                if(end<n-1 && start>0 && Character.isLowerCase(s.charAt(start-1))&& Character.isLowerCase(s.charAt(end+1))){
+                    count++;
+                }
+
+
+            }
+            else{
+                i++;
+            }
+         }
+         System.out.println(count);
     }
 }
