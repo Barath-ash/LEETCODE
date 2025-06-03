07916f1a5670202de72fdb252b1c9ca969dc158d
Barath-ash <bbarath601@gmail.com>
2025-06-04 03:52:57 +0530

    Practice questions

--- maximumsubaray.java
+++ maximumsubaray.java
@@ -1,24 +1,24 @@
 
-// class maximumsubaray{
-//     public static void main(String[] args) {
-//         int arr[]= {-3,4,-2,5,3,-2,8,2,1,4};
-//         int n = arr.length;
-//         int k = 5;
-//         int sum =0;
-//         int max =0;
-//         for(int i=0;i<k;i++){
-//             sum+=arr[i];
-//         }
-//         max = sum;
+class maximumsubaray{
+    public static void main(String[] args) {
+        int arr[]= {-3,4,-2,5,3,-2,8,2,1,4};
+        int n = arr.length;
+        int k = 5;
+        int sum =0;
+        int max =0;
+        for(int i=0;i<k;i++){
+            sum+=arr[i];
+        }
+        max = sum;
 
-//         for(int i=1;i<=n-k;i++){
-//             sum+=arr[i+k-1];
-//             sum-=arr[i-1];
-//             if(sum>max){
-//                 max = sum;
-//             }
+        for(int i=1;i<=n-k;i++){
+            sum+=arr[i+k-1];
+            sum-=arr[i-1];
+            if(sum>max){
+                max = sum;
+            }
                
-//         }
-//         System.out.println(max);
-//     }
-// }
+        }
+        System.out.println(max);
+    }
+}
